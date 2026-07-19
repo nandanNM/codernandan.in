dda27de56a5ecfc8a3859d3d8c4025f7e831aa17
Nandan Manna <codernandan@gmail.com>
2026-07-19 06:32:50 +0530

    Add new Oneko cat sprites and update OnekoCat component to use random sprite

--- src/components/common/OnekoCat.tsx
+++ src/components/common/OnekoCat.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import Script from "next/script";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { catConfig } from "@/config/cat";
 
+const CAT_SPRITES = [
+  "./oneko/oneko.gif",
+  "./oneko/oneko-maia.gif",
+  "./oneko/oneko-tora.gif",
+];
+
 export default function OnekoCat() {
-  if (!catConfig.enabled) {
+  const [cat, setCat] = useState<string | null>(null);
+
+  useEffect(() => {
+    setCat(CAT_SPRITES[Math.floor(Math.random() * CAT_SPRITES.length)]);
+  }, []);
+
+  if (!catConfig.enabled || !cat) {
     return null;
   }
 
-  return <Script src="./oneko/oneko.js" data-cat="./oneko/oneko.gif" />;
+  return <Script src="./oneko/oneko.js" data-cat={cat} />;
 }
